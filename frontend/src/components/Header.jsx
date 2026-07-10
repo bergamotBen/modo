a373f46ec3e4909e36621ec0399f20280f293b2d
Ben <bergamotBen@icloud.com>
2026-07-10 22:40:17 +0100

    Breatime component created

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,18 +2,18 @@ import Container from "react-bootstrap/esm/Container";
 import { useState, useEffect } from "react";
 
 function Timer() {
-  const [time, setTime] = useState(25);
+  const [time, setTime] = useState(2);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime((prevTime) => {
-        if (prevTime === 0) {
+        if (prevTime === 1) {
           clearInterval(interval);
-          return "DONE";
+          return <br />;
         }
         return prevTime - 1;
       });
-    }, 60000);
+    }, 3000);
 
     return () => {
       if (interval) clearInterval(interval);
@@ -24,16 +24,14 @@ function Timer() {
 export default function Header({ title }) {
   if (title) {
     return (
-      <Container className="text-end p-2 px-lg-3">
+      <Container className="text-end mt-2 p-2 px-lg-3">
         <h1>{title}</h1>
       </Container>
     );
   } else {
     return (
-      <Container className="text-end p-2 px-lg-3">
-        <h3>
-          <Timer />
-        </h3>
+      <Container className="text-end mt-2 p-2 px-lg-3 h1">
+        <Timer />
       </Container>
     );
   }
--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -6,19 +6,31 @@ export default function Task({
   showPosition,
   showButtons,
   showDetails,
+  active,
 }) {
-  let cardClass = "py-3 mx-2 mx-lg-5";
+  let cardClass = "py-3 mx-2 mx-lg-5 mb-3";
+  let border = "";
+  let textClassName = "text-start px-3";
+
   if (showButtons) {
-    cardClass = "pt-3 mx-2 mx-lg-5";
+    cardClass = "pt-3 mx-2 mx-lg-5 mb-3";
   }
+
+  if (active) {
+    border = "success";
+    textClassName += " text-success";
+  }
+
   return (
-    <Card className={cardClass}>
+    <Card border={border} className={cardClass}>
       {showPosition ? (
         <Card.Title className="text-start px-3">#1</Card.Title>
       ) : null}
-      <Card.Text className="text-start px-3">{text}</Card.Text>
+      <Card.Text className={textClassName}>{text}</Card.Text>
 
-      {showDetails ? <div className="text-end px-3">{details}</div> : null}
+      {showDetails ? (
+        <div className="text-end px-3 text-secondary">{details}</div>
+      ) : null}
       {showButtons ? (
         <Card.Footer className="text-end">⬆️ ⬇️ 🗑️</Card.Footer>
       ) : null}
