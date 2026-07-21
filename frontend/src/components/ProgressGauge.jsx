341d40e76181709aecb24d4015a2016b65f221fa
Ben <bergamotBen@icloud.com>
2026-07-21 17:26:28 +0100

    today/week count only applies to complete=true, front end displays accurate stats

--- frontend/src/components/ProgressGauge.jsx
+++ frontend/src/components/ProgressGauge.jsx
@@ -1,9 +1,38 @@
 import Card from "react-bootstrap/esm/Card";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+import { useOutletContext } from "react-router-dom";
 
-export default function ProgressGauge({ today, value }) {
+export default function ProgressGauge({ today }) {
+  const [stats, setStats] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const { userId } = useOutletContext();
+
+  useEffect(() => {
+    async function fetchStats() {
+      setLoading(true);
+      const range = today ? "today_count" : "week_count";
+      const { data, error } = await supabase
+        .from("user_task_stats")
+        .select(range)
+        .eq("user", userId)
+        .maybeSingle();
+
+      if (error) {
+        console.error(`Error fetching ${range}: ${error}`);
+      } else if (data) {
+        console.log(data);
+        setStats(data[range]);
+      }
+      setLoading(false);
+    }
+    if (userId) {
+      fetchStats();
+    }
+  }, [userId]);
   return (
     <Card>
-      <Card.Body className="h1 text-center m-0">{value}</Card.Body>
+      <Card.Body className="h1 text-center m-0">{stats}</Card.Body>
       {today ? (
         <Card.Footer className="text-center">TODAY</Card.Footer>
       ) : (
