646313b0afb5b84991261ef2e5c0e718b2d262f0
Ben <bergamotBen@icloud.com>
2026-07-25 13:48:21 +0100

    trashcan button works. Fixes #62 and #63

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,6 +19,7 @@ export default function Home() {
           complete: false,
           orderBy: "priority",
           ascending: true,
+          archived: false,
         });
 
         const activeTask = allTasks.find((task) => task.active === true);
