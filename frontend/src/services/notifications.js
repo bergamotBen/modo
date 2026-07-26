15d5eb5ca14a628010717b22293241fe84e94520
Ben <bergamotBen@icloud.com>
2026-07-26 12:12:29 +0100

    removed unused code

--- frontend/src/services/notifications.js
+++ frontend/src/services/notifications.js
@@ -1,22 +1,5 @@
 import { supabase } from "../lib/supabase";
 
-export async function schodooeduleNotification(messageText, delayInMinutes) {
-  // Calculate target time
-  const scheduledTime = new Date();
-  scheduledTime.setMinutes(scheduledTime.getMinutes() + delayInMinutes);
-
-  // Insert row into table
-  const { data, error } = await supabase.from("scheduled_pushes").insert([
-    {
-      scheduled_for: scheduledTime.toISOString(),
-      payload: {
-        title: "Scheduled Alert!",
-        body: messageText,
-      },
-    },
-  ]);
-}
-
 export async function schedulePush(userId, duration, title, body) {
   const time = new Date(Date.now() + duration * 100).toISOString();
   const { data, error } = await supabase
